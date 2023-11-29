329761b451070ec50698d62bff82845fab8ea640
EndiMarani <130464771+EndiMarani@users.noreply.github.com>
2023-11-29 09:09:47 -0300

    allowed_hosts permitido para todos

--- projeto_distribueasy/projeto_distribueasy/settings.py
+++ projeto_distribueasy/projeto_distribueasy/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.109.238','192.168.109.234','192.168.109.251','192.168.109.241','192.168.109.240', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
